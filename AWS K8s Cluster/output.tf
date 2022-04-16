a4aba6423a3787e6f0ca64cd46c3fa1853d1b052
Yomna-Meles <63149080+Yomna-Meles@users.noreply.github.com>
2022-04-16 22:56:22 +0200

    Add files via upload

---
+++ AWS K8s Cluster/output.tf
@@ -0,0 +1,5 @@
+output "cluster_id" {
+    value = module.eks.cluster_id
+}output "cluster_endpoint" {
+    value = module.eks.cluster_endpoint
+}
---
+++ AWS K8s Cluster/vars.tf
@@ -0,0 +1,5 @@
+variable "access_key" {
+  default = "<Your-AWS-Access-Key>"
+}variable "secret_key" {
+    default = "<Your-AWS-Secret-Key>"
+}
