3c841b8778d7cbccdf420a728be905b22b0d20c8
Yomna-Meles <63149080+Yomna-Meles@users.noreply.github.com>
2022-04-18 16:53:32 +0200

    Add files via upload

---
+++ AWS K8s Cluster/NewCluster/cluster.tf
@@ -0,0 +1,14 @@
+locals {
+  cluster_name = "my-eks-cluster"
+}
+module "vpc" {
+  source = "git::https://git@github.com/reactiveops/terraform-vpc.git?ref=v5.0.1"
+
+  aws_region = "us-east-1"
+  az_count   = 3
+  aws_azs    = "us-east-1a, us-east-1b, us-east-1c"
+
+  global_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+}
